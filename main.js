719e17a16c92a57748455d0e3fbcfa9e11623d75
지로쿨 <cse96hk@gmail.com>
2024-07-09 11:59:23 +0900

    api 6강

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const API_KEY = `7713fec458914cb7b0bf43b12d8bd292`;
 let news = [];
 const getLastNews = async () => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    //const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    // noona API 배포에만 사용
+    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
     const response = await fetch(url);
     const data = await response.json();
     news = data.articles;
