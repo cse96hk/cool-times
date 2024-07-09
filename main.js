efd9ac6c5eee77b9b2e29e321b78bcb028d13e96
지로쿨 <cse96hk@gmail.com>
2024-07-09 13:57:50 +0900

    api 6강

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const API_KEY = `7713fec458914cb7b0bf43b12d8bd292`;
 const currentUrl = window.location.href;
 let newsApiUrl = "";
 // 개발시 / 배포시 에 따라 api url 처리
-if (currentUrl === "https://cool-times.netlify.app") {
-    newsApiUrl = `https://noona-times-v2.netlify.app/top-headlines`;
-} else {
+if (currentUrl === "http://127.0.0.1:5500/") {
     newsApiUrl = `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`;
+} else {
+    newsApiUrl = `https://noona-times-v2.netlify.app/top-headlines`;
 }
 let news = [];
 const getLastNews = async () => {
