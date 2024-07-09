ab7330fbfaf8d7a58bb291409cd819e22baea6a2
지로쿨 <cse96hk@gmail.com>
2024-07-09 12:35:59 +0900

    api 6강

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+const currentUrl = window.location.href;
+console.log(currentUrl);
 const API_KEY = `7713fec458914cb7b0bf43b12d8bd292`;
+const localApiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+const globalApiUrl = `https://noona-times-v2.netlify.app/top-headlines`;
 let news = [];
 const getLastNews = async () => {
-    //const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-    // noona API 배포에만 사용
-    const url = new URL(`https://noona-times-v2.netlify.app/top-headlines`);
+    const url = new URL(globalApiUrl);
 
     const response = await fetch(url);
     const data = await response.json();
