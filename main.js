25e3343a6b814d36b6f7a413e130427eca0274d6
지로쿨 <cse96hk@gmail.com>
2024-07-10 16:58:07 +0900

    api 8강

--- main.js
+++ main.js
@@ -18,6 +18,17 @@ const getLastNews = async () => {
     renderNews();
     console.log(newsList);
 };
+const formatDateTime = (inputDateTime) => {
+    const date = new Date(inputDateTime);
+    console.log(date);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    return `${year}.${month}.${day} ${hours}:${minutes}`;
+};
 
 const renderNews = () => {
     const newsHTML = newsList
@@ -28,8 +39,8 @@ const renderNews = () => {
                 <img src="${news.urlToImage}" onerror="this.onerror=null; this.src='https://unsplash-assets.imgix.net/empty-states/photos.png?auto=format&fit=crop&q=100'" class="card-img-top"  >
                 <div class="card-body">
                     <h5 class="card-title">${news.title}</h5>
-                    <p class="card-text">${news.description}</p>
-                    <p class="card-text">${news.source.name} * ${news.publishedAt}</p>
+                    <p class="card-text">${news.description == null ? " " : news.description}</p>
+                    <p class="card-text">${news.source.name} * ${formatDateTime(news.publishedAt)}</p>
                     <a href="${news.url}" class="btn btn-light" target="_blank">기사링크</a>
                 </div>
             </div>
